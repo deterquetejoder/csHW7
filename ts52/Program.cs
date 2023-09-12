77039855b6abf0592657827091a59de841dfa929
deterquetejoder <saecularisator@gmail.com>
2023-09-12 23:49:28 +0500

    done

--- ts52/Program.cs
+++ ts52/Program.cs
@@ -1,2 +1,30 @@
 ﻿// Задача 52. Задайте двумерный массив из целых чисел. 
 // Найдите среднее арифметическое элементов в каждом столбце.
+
+void PrintArray(double[] averages)
+{
+    for(int i=0;i<averages.Length;i++)
+    {
+        Console.WriteLine(averages[i]);
+    }
+}
+
+int[,] arr = new int[,] {
+    {1, 2},
+    {3, 4},
+    {5, 6}
+};
+
+
+double[] averages = new double[arr.GetLength(0)];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+ {
+    double sum = 0.0;
+    for (int j = 0; j < arr.GetLength(1); j++) {
+        sum += arr[i, j];
+    }
+    averages[i] = sum / arr.GetLength(1);
+}
+
+PrintArray(averages);
