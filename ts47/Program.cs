0471a9d35a38c2247f07f94e04e7e6f52f7c5824
deterquetejoder <saecularisator@gmail.com>
2023-09-12 21:47:26 +0500

    ts47 done

--- ts47/Program.cs
+++ ts47/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный
+//  случайными вещественными числами.
+  
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Размеры массива
+        int m = 3;
+        int n = 4;
+
+        // Инициализация двумерного массива
+        double[,] array = new double[m, n];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = rnd.NextDouble();
+                Console.Write($"{array[i, j]:F2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
